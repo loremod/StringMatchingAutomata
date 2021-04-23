d631c5aac535064c1402134eceb38775729c314d
loremod <56656615+loremod@users.noreply.github.com>
2021-04-23 11:39:43 +0200

    Rename suffixes.h to src/suffixes.h

--- src/suffixes.h
+++ src/suffixes.h
@@ -1,13 +1,13 @@
 
 using namespace std;
-//confronta due stringhe: ritorna true se a è suffisso di b, 0 altrimenti
+//confronta due stringhe: ritorna true se a Ã¨ suffisso di b, 0 altrimenti
 bool isSuffix(string a, string b){
 	for(int i = 1; i <= a.length(); i++)
 		if(a.at(a.length() - i) != b.at(b.length() - i))
 			return false;
 	return true;
 }
-//Trova la dimensione della più grande sottostringa di sf che è suffissa di p
+//Trova la dimensione della piÃ¹ grande sottostringa di sf che Ã¨ suffissa di p
 int bestSubSuffix(string sf, string p){
 	for(int i = sf.length(); i > 0; i--)
 		if(isSuffix(sf.substr(0,i), p))
