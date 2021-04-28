c6e02a7773d09ad98d20e6fbb372e2d6a39b4e54
loremod <56656615+loremod@users.noreply.github.com>
2021-04-28 20:49:16 +0200

    Create samplesManager.cpp

---
+++ src/util/samplesManager.cpp
@@ -0,0 +1,19 @@
+//#ifstream <ifstream>
+//#include <filesystem>
+//namespace fs = std::filesystem;
+//class samplesManager{
+//	private:
+//		string text;
+//	
+//};
+#include <string>
+#include <iostream>
+#include <filesystem>
+namespace fs = std::filesystem;
+
+int main()
+{
+    std::string path = "/path/to/directory";
+    for (const auto & entry : fs::directory_iterator(path))
+        std::cout << entry.path() << std::endl;
+}
