aa1d42f8e9dadc5cf012bf3436eb4364a328d7e7
loremod <56656615+loremod@users.noreply.github.com>
2021-04-28 19:32:53 +0200

    Adding possibility to compare algorithms

---
+++ src/compare_algorithms/compare.cpp
@@ -0,0 +1,87 @@
+#include <chrono>
+#include "../string_matching_automata.h"
+#include "../naive_algorithm/naive_string_matching.h"
+#include "../util/useful_code.h"
+
+#define NUM_FUNC	2 //numero di funzioni da confrontare
+
+using namespace std::chrono;
+
+//template<class T> void ask(string question, T& response);
+//template<class T> void showVector(vector<T>* v);
+vector<int>* automata_string_matching(string T, string p);
+void fetch_function_results(vector<int>** where_to_store, vector<int>* (**functions_to_execute)(string,string), string T, string p);
+bool checkCorrectness(vector<int>**);
+
+vector<int>* (*string_matching[NUM_FUNC])(string, string) {naive_string_matching, automata_string_matching};
+string funcs[NUM_FUNC] = {"Naive algorithm","Finite Automata algorithm"};
+vector<int>* f_results[NUM_FUNC];
+double f_times[NUM_FUNC];
+
+int main(){
+	
+	//Titolo e descrzione
+	cout << "\nConfronto tra algoritmi di string matching\n\n";
+	cout << "Gli algoritmi considerati sono: ";printArray(funcs,NUM_FUNC,"; ");
+	cout << endl << endl;
+	
+	//Richiedere Testo e pattern
+	string T,p;
+ 	ask("Inserisci testo: ", T);
+	ask("Inserisci pattern: ",p);
+	
+	//Eseguire gli algoritmi e ottenere output e tempi di esecuzione
+	fetch_function_results(f_results,string_matching,T,p);
+	
+	//Naive
+	cout << endl << funcs[0] <<":\n";
+	showVector(f_results[0]);
+	
+	//Automata
+	cout << endl << funcs[1] <<":\n";
+	showVector(f_results[1]);
+	
+	//Correctness
+	if(checkCorrectness(f_results))cout << "\nGli algoritmi hanno prodotto lo stesso risultato";
+	else	cout << "\nI valori non coincidono";
+	
+	//Comparing functions time execution 
+}
+
+vector<int>* automata_string_matching(string T, string p){
+	SMA sma(p);
+	return sma.getMatches(T);
+}
+
+void fetch_function_results(vector<int>** where_to_store, vector<int>* (**functions_to_execute)(string,string), string T, string p){
+	for(int i = 0 ; i < NUM_FUNC ; i++){
+		auto start = high_resolution_clock::now();
+		where_to_store[i] = functions_to_execute[i](T, p);
+		auto stop = high_resolution_clock::now();
+		auto duration = duration_cast<microseconds>(stop - start);
+		cout << duration.count() << endl;
+	}
+}
+bool checkCorrectness(vector<int>** sequences){
+	//Controllare le dimensioni
+	for(int i = 0 ; i < NUM_FUNC ; i++)
+		if(sequences[i]->size() != sequences[0]->size()) return false;
+	//Controllare i valori(con l'ipotesi che siano gia' ordinati)
+	for(int j = 0 ; j < sequences[0]->size() ; j++)
+		for(int i = 0 ; i < NUM_FUNC ; i++)
+			if(sequences[0]->at(j) != sequences[i]->at(j))	return false;
+	return true;
+}
+
+//template<class T>
+//void ask(string question, T& response){
+//	cout << question;
+//	getline(cin, response);
+//}
+//template<class T> 
+//void showVector(vector<T>* v){
+//	for(const auto &e : *v)
+//		cout << e << "	";
+//	cout << endl;
+//}
+
---
+++ src/util/useful_code.h
@@ -0,0 +1,29 @@
+//Delle funzioni per raggruppare codice utile
+
+template<class T>
+void ask(string question, T& response){
+	cout << question;
+	getline(cin, response);
+}
+
+template<class T> 
+void printVectorElements(vector<T>* v){
+	for(const auto &e : *v)
+		cout << e << "	";
+	cout << endl;
+}
+
+template<class T>
+void showVector(vector<T>* v){
+	cout << "Number of occurences: " << v->size() << endl;
+	if(v->size()){
+		cout << "Occurences: ";
+		printVectorElements(v);
+	}
+}
+
+template<class T>
+void printArray(T* arr, int len, string separator=" "){
+	for(int i = 0 ; i < len ; i++) 
+		cout << *(arr + i) << separator;
+}
