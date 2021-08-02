a05a6d07154cb2d1d8a4bb3691edc1c0f4dca0a7
loremod <56656615+loremod@users.noreply.github.com>
2021-08-02 11:23:10 +0200

    Update naive_string_matching.h

--- src/naive_algorithm/naive_string_matching.h
+++ src/naive_algorithm/naive_string_matching.h
@@ -7,8 +7,8 @@ bool string_match(string a, string b){
 	return (!a.compare(b));
 }
 
-vector<int>* naive_string_matching(string T, string p){//T: testo, p: stringa da trovare nel testo
-	vector<int>* sol = new vector<int>();
+vector<int>* naive_string_matching(string T, string p){ //T: testo, p: stringa da trovare nel testo
+	vector<int>* sol = new vector<int>();				//Vettore delle soluzioni
 	int n = T.length();
 	int m = p.length();
 //	if(!n or !m) return NULL;//nel caso in cui una delle due stringhe sia vuota
